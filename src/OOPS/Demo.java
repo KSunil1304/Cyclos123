d858fff7d46d5de2183beea36bb83c3b5483e9b5
KSunil1304 <kommayellasunil@gmail.com>
2026-04-25 12:52:43 +0530

    Update Demo.java

--- src/OOPS/Demo.java
+++ src/OOPS/Demo.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	Interface_First myobj = new Interface_First();
 	myobj.FirstMethod();
 	System.out.println(myobj);
+	//This is the updated comment from web and committed the changes
 }
 }
